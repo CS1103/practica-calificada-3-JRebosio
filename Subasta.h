6ba81eccf246fb9527f3e35dfd0542b4a52fc6e8
Jorge Rebosio <jorge.rebosio@utec.edu.pe>
2019-11-07 18:13:30 -0500

    print

--- Subasta.h
+++ Subasta.h
@@ -10,6 +10,7 @@
 #include <sstream>
 #include <map>
 #include <set>
+#include <vector>
 using namespace std;
 
 
@@ -77,9 +78,16 @@ public:
     }
 
     void Save(std::ofstream& myfile){
+        Erase();
+        auto it=m.begin();
+        for (auto &it : m) {
+            myfile<<it
+        }
 
 
-    }
+
+        }
+
 
 
 private:

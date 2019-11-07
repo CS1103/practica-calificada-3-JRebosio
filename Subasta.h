8582458110cf6f6721c7411f27f3da9d397947e5
Jorge Rebosio <jorge.rebosio@utec.edu.pe>
2019-11-07 18:04:04 -0500

    eaxmen

---
+++ Subasta.h
@@ -0,0 +1,93 @@
+
+#ifndef PC3_SUBASTA_H
+#define PC3_SUBASTA_H
+
+
+#include <cctype>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <set>
+using namespace std;
+
+
+
+struct contestant {
+    string name;
+    int money;
+    bool operator <(contestant other)const{
+        return money<other.money;
+    }
+
+};
+map<string, set<contestant>> m;
+map<string, int> counts;
+
+
+
+void Erase() {
+    for (auto &u : m) {
+        for (auto i = u.second.begin(); i != u.second.end();) {
+            auto x=*i;
+            if (counts[x.name] > 1) u.second.erase(i++);
+            else ++i;
+        }
+    }
+}
+
+bool isproduct(const string& word)
+{
+    for(auto& c: word)
+        if(!isspace(static_cast<unsigned char>(c)))
+            if(!isupper(static_cast<unsigned char>(c)))
+                return false;
+    return true;
+}
+
+class Subasta{
+public:
+    Subasta(string file) {
+        ifstream s(file);
+        string producto;
+        getline(s, producto);
+        int l = 1;
+        while (l != 5) {
+            if (isproduct(producto)) {
+                string of=producto;
+                getline(cin, producto);
+                stringstream ss(producto);
+                string name;
+                int money;
+                m[of];
+                while (!isproduct(producto)) {
+                    ss >> name >> money;
+                    contestant temp;
+                    temp.name=name;
+                    temp.money=money;
+                    m[of].insert(temp);
+                    getline(cin,producto);
+                }
+                for (auto &it : m[of]) counts[it.name]++;
+                l++;
+            }
+
+        }
+    }
+
+    void Save(std::ofstream& myfile){
+
+
+    }
+
+
+private:
+
+
+
+};
+
+
+
+#endif //PC3_SUBASTA_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "Subasta.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Subasta subasta{"bif_example.txt"};
+    ofstream myfile;
+    myfile.open("resul_example.txt");
+    subasta.Save()myfile;
     return 0;
 }
